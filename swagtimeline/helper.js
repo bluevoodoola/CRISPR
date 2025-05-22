92bab4b1332730ca8cb03444f86b6b0832b286b8
Cade Roux <cade@roux.org>
2025-05-22 08:34:14 -0500

    Function move

--- swagtimeline/helper.js
+++ swagtimeline/helper.js
@@ -28,17 +28,3 @@ function createElement(tag, options = {}) {
 
   return element;
 }
-
-function createAnomalyTimeline(accordion, anomaly) {
-    accordion
-        .appendChild(createElement("h2", { attributes: { id: "h2#" + anomaly.series.handle } }))
-        .appendChild(createElement("a", { attributes: { id: "#" + anomaly.uniqueid, href: "#" + anomaly.uniqueid }, classes: [ "header" ], textContent: anomaly.header }));
-    accordion.appendChild(createElement("div", { attributes: { id: "vis" + anomaly.uniqueid } }));
-
-    const tml = new vis.Timeline(
-        document.getElementById("vis" + anomaly.uniqueid)
-        , new vis.DataSet(anomaly.schedule_swag.events)
-        , new vis.DataSet(anomaly.schedule_swag.groups)
-        , {}
-    );
-}
--- swagtimeline/swagtimeline.js
+++ swagtimeline/swagtimeline.js
@@ -1,10 +1,22 @@
 var accordion = document.getElementById("accordion");
+
+function createAnomalyTimeline(accordion, anomaly) {
+    accordion
+        .appendChild(createElement("h2", { attributes: { id: "h2#" + anomaly.series.handle } }))
+        .appendChild(createElement("a", { attributes: { id: "#" + anomaly.uniqueid, href: "#" + anomaly.uniqueid }, classes: [ "header" ], textContent: anomaly.header }));
+    accordion.appendChild(createElement("div", { attributes: { id: "vis" + anomaly.uniqueid } }));
+
+    const tml = new vis.Timeline(
+        document.getElementById("vis" + anomaly.uniqueid)
+        , new vis.DataSet(anomaly.schedule_swag.events)
+        , new vis.DataSet(anomaly.schedule_swag.groups)
+        , {}
+    );
+}
+
 ingress.anomalies.forEach(anomaly => {
-    {
-        createAnomalyTimeline(
-            accordion
-            , anomaly
-        );
-    }
-    ;
+    createAnomalyTimeline(
+        accordion
+        , anomaly
+    );
 });

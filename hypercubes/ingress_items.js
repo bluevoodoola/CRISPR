2b70b815a51eb28febe78b40f3c85ddd80183062
Cade Roux <cade@roux.org>
2025-06-04 13:30:50 -0500

    Refactoring

---
+++ hypercubes/ingress_items.js
@@ -0,0 +1,30 @@
+const INGRESS_ITEMS = {
+	STATS_POWERCUBE: {
+		"1": {Level: 1, XM: 1000},
+		"2": {Level: 2, XM: 2000},
+		"3": {Level: 3, XM: 3000},
+		"4": {Level: 4, XM: 4000},
+		"5": {Level: 5, XM: 5000},
+		"6": {Level: 6, XM: 6000},
+		"7": {Level: 7, XM: 7000},
+		"8": {Level: 8, XM: 8000}
+	}
+	, STATS_HYPER: {
+		"1": {Level: 1, XM: 18000},
+		"2": {Level: 2, XM: 20250},
+		"3": {Level: 3, XM: 22500},
+		"4": {Level: 4, XM: 24750},
+		"5": {Level: 5, XM: 27000},
+		"6": {Level: 6, XM: 29250},
+		"7": {Level: 7, XM: 31500},
+		"8": {Level: 8, XM: 33750},
+		"9": {Level: 9, XM: 36000},
+		"10": {Level: 10, XM: 38400},
+		"11": {Level: 11, XM: 40800},
+		"12": {Level: 12, XM: 43200},
+		"13": {Level: 13, XM: 45600},
+		"14": {Level: 14, XM: 48000},
+		"15": {Level: 15, XM: 50400},
+		"16": {Level: 16, XM: 52800}
+	}
+};
